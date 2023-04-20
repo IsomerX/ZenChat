9ed666710624f9415650547e1b9622012f8fab3e
IsomerX <49413460+IsomerX@users.noreply.github.com>
2023-04-20 09:09:05 +0530

    changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,43 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import localFont from "@next/font/local";
 
 import "~/styles/globals.css";
+
+const loubag = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Loubag-Black.ttf",
+      weight: "900",
+    },
+    {
+      path: "../../public/fonts/Loubag-ExtraBold.ttf",
+      weight: "800",
+    },
+    {
+      path: "../../public/fonts/Loubag-Bold.ttf",
+      weight: "700",
+    },
+    {
+      path: "../../public/fonts/Loubag-Medium.ttf",
+      weight: "500",
+    },
+    {
+      path: "../../public/fonts/Loubag-Regular.ttf",
+      weight: "400",
+    },
+    {
+      path: "../../public/fonts/Loubag-Light.ttf",
+      weight: "300",
+    },
+    {
+      path: "../../public/fonts/Loubag-Thin.ttf",
+      weight: "200",
+    },
+  ],
+  variable: "--font-loubag",
+});
+export const loubagFontClassname = loubag.className;
+export const loubagFontVariable = loubag.variable;
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,19 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        a: "#F89C6B",
+        b: "#F6E8DD",
+        c: "#3A597B",
+        d: "#79CAC4",
+        e: "#89023E",
+      },
+      fontFamily: {
+        loubag: ["var(--font-loubag)", "sans-serif"],
+        pop: ["'Poppins', sans-serif"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
